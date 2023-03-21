5c10342c80b3f04d5286ee21f014f6268064fe1c
VeeraKonabhai <veerareddy908@gmail.com>
2023-03-21 23:15:21 +0530

    added changes to communicate with backend proj

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # vue-boot-DanVega-YT
 
-This is based and created using teh below video available on youtube :
+This is based and created using the below video available on youtube :
 https://www.youtube.com/watch?v=5mpHejytgFE
 
 
--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,15 +1,33 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- removing below 2 lines and lines in <script> to just show the response we get from backend project -->
+  <!-- <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <h1>{{ title }}</h1>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+
+// import { response } from 'express';
 
 export default {
   name: 'App',
-  components: {
+  /* components: {
     HelloWorld
+  } */
+  data(){ //new data property function to display message(return object) in above <h1>
+    return {
+      title: ''
+    }
+  },
+  mounted(){  //reachout to backend project and get the text we gave over there
+    fetch('http://localhost:8085/veera') //url from where we want data
+    .then(response => {
+      return response.text(); //since we are just sending string in backend, we are using text() here or else we can have JSON object methods
+    })
+    .then(data =>{
+      this.title = data; //setting teh title with data we received
+    });
   }
 }
 </script>
